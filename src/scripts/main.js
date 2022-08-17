0d52d4e23fcbc45b8f29e7367ff27e0cdbfbb50a
sergii-nosachenko <s.nosachenko@gmail.com>
2022-08-17 16:49:54 +0300

    Task solution code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,53 @@
 'use strict';
 
-// write your code here
+const apiUrl = 'https://mate-academy.github.io/phone-catalogue-static/api';
+
+const request = (endpoint, params = {}) => {
+  return fetch(apiUrl + endpoint, params)
+    .then(response => {
+      if (!response.ok) {
+        return Promise.reject(
+          new Error(`
+            ${response.status}: ${response.statusText}
+          `)
+        );
+      }
+
+      return response.json();
+    });
+};
+
+const getPhones = () => {
+  return request('/phones.json');
+};
+
+const getDetails = (phoneId) => {
+  return request(`/phones/${phoneId}.json`);
+};
+
+const getPhonesDetails = () => {
+  return getPhones()
+    .then(phones => {
+      const getDetailsPromises = [];
+
+      phones.forEach(({ id }) => getDetailsPromises.push(getDetails(id)));
+
+      return Promise.all(getDetailsPromises);
+    });
+};
+
+getPhonesDetails()
+  .then(allPhones => {
+    const list = document.createElement('ul');
+
+    document.body.append(list);
+
+    allPhones.forEach(phone =>
+      list.insertAdjacentHTML('beforeend', `
+        <li>${phone.name}</li>
+      `)
+    );
+  })
+  .catch(error => {
+    alert(error.message);
+  });
